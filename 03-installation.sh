332f7bb6ca42d09f30d023cf7952f1e26fc59dd8
yuvaraj <yuvarajreddy24@gmail.com>
2025-09-22 22:47:42 +0530

    03-installation.sh

--- 03-installation.sh
+++ 03-installation.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if  [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run the script with root user"
+    exit 1 #failure is other than 0
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR:: Installing mysql is failure"
+    exit 1
 else
-echo "Installing mysql is SUCCESS"
+    echo "Installing mysql is SUCCESS"
 fi
