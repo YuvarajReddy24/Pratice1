5528f5f31ff66f62544ba41715faa9f278684afc
yuvaraj <yuvarajreddy24@gmail.com>
2025-10-02 22:17:50 +0530

    .

--- 07-colours.sh
+++ 07-colours.sh
@@ -14,10 +14,10 @@ fi
 
 VALIDATE(){ #functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e "ERROR:: Installing $2.....$R failure $N"
+        echo -e "ERROR:: Installing $2...$R failure $N"
         exit 1
     else
-         echo -e "Installing $2..... $G SUCCESS $N"
+         echo -e "Installing $2...$G SUCCESS $N"
     fi
 }
 dnf list installed mysql
